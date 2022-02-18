d7c1169930ea7048becbaa93101584aaab7a9de3
Nathan Heidacker <nathanheidacker@gmail.com>
2022-02-18 12:29:31 +0900

    tabs to spaces

--- AlphaGradient/algolib/lib.py
+++ AlphaGradient/algolib/lib.py
@@ -2,7 +2,7 @@
 """AlphaGradient's algorithm library, containing vetted and high quality financial algorithms to be used freely.
 
 Todo:
-	* NA
+    * NA
 """
 
 # Standard Imports
@@ -18,41 +18,41 @@ import AlphaGradient as ag
 
 class SpyCoveredCalls(ag.Algorithm):
 
-	def setup(self, start, end):
-		env = ag.finance.Basket(start=start)
-		self.spy = env.stock("SPY")
-		self.p = env.portfolio(self.spy.value * 101)
+    def setup(self, start, end):
+        env = ag.finance.Basket(start=start)
+        self.spy = env.stock("SPY")
+        self.p = env.portfolio(self.spy.value * 101)
 
-		return env
+        return env
 
-	def run(self, start, end):
-		day = 1
-		while self.date < end:
-			
-			# Buying spy if we can buy spy
-			to_buy = math.floor((self.p.liquid / self.spy.value) / 100) * 100
-			if to_buy > 0:
-				self.env.buy(self.spy, to_buy)
+    def run(self, start, end):
+        day = 1
+        while self.date < end:
+            
+            # Buying spy if we can buy spy
+            to_buy = math.floor((self.p.liquid / self.spy.value) / 100) * 100
+            if to_buy > 0:
+                self.env.buy(self.spy, to_buy)
 
-			# Selling calls if we can sell calls
-			position = self.p.longs.get("STOCK_SPY", False)
-			if position and position.quantity >= 100:
-				to_sell = math.floor(position.quantity / 100)
-				self.env.short(self.spycall(), to_sell)
+            # Selling calls if we can sell calls
+            position = self.p.longs.get("STOCK_SPY", False)
+            if position and position.quantity >= 100:
+                to_sell = math.floor(position.quantity / 100)
+                self.env.short(self.spycall(), to_sell)
 
-			# Go to the next trading day
-			self.verbose(f"DAY {day}")
-			self.verbose(self.p.positions, "\n")
-			self.env.step()
-			day += 1
+            # Go to the next trading day
+            self.verbose(f"DAY {day}")
+            self.verbose(self.p.positions, "\n")
+            self.env.step()
+            day += 1
 
-	def spycall(self, offset=1, delta=1):
-		strike = self.spy.value + offset
-		call = self.env.call(self.spy, strike, delta)
-		return call
-
-
+    def spycall(self, offset=1, delta=1):
+        strike = self.spy.value + offset
+        call = self.env.call(self.spy, strike, delta)
+        return call
 
 
 
-		
+
+
+        
