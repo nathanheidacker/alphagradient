624d1b050bab6cbfd054c1265b858120db1ac0c7
Nathan Heidacker <nathanheidacker@gmail.com>
2022-04-17 04:36:56 +0900

    github automated testing setup

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -3,21 +3,12 @@ import unittest
 import os
 import sys
 import inspect
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
-
-import AlphaGradient as ag
+import alphagradient as ag
 
 class Standard(unittest.TestCase):
 
     def test_stock(self):
-        # spy = ag.finance.Stock('SPY', date=datetime.today())
-        # print(spy.price)
-
         self.assertEquals(1, 1)
-        # self.assertEquals(spy.price, 430.429993)
 
 if __name__ == '__main__':
     unittest.main()
